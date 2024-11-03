575a99ce0963f7435ef8993864f04ef298220ea9
Istrate-Mihai <i.mihai9960@gmail.com>
2024-11-03 21:27:28 +0200

    Replaced temporary data storage with MongoDB Mlab Service

--- server.js
+++ server.js
@@ -3,24 +3,46 @@ const app        = express();
 const bodyParser = require('body-parser');
 const http       = require('http').Server(app);
 const io         = require('socket.io')(http);
+const mongoose   = require('mongoose');
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.json());
 
-const messageList = [
-  {name: 'Tim', message: 'Hi',},
-  {name: 'Jane', message: 'Hello',},
-];
+const dbUrl = "mongodb+srv://mihai:1234@cluster0.ag6ww.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-app.get('/messages', (req, res) => {
-  res.send(messageList);
+const Message = mongoose.model('Message', {
+  name: String,
+  message: String,
 });
 
-app.post('/messages', (req, res) => {
-  messageList.push(req.body);
-  io.emit('message', req.body);
-  res.sendStatus(200);
+mongoose.connect(dbUrl).then(() => {
+  console.log('MongoDB connected successfully');
+}).catch(err => {
+  console.error('MongoDB connection error:', err);
+});
+
+// Route to get all messages
+app.get('/messages', async (req, res) => {
+  try {
+      const messages = await Message.find();
+      res.status(200).json(messages);
+  } catch (error) {
+      res.status(500).json({ message: 'Error fetching messages', error });
+  }
+});
+
+app.post('/messages', async (req, res) => {
+  try {
+    const message = await new Message(req.body);
+    message.save();
+    io.emit('message', req.body);
+    res.status(200);
+  }
+  catch (err) {
+    res.status(500);
+  }
 });
 
 io.on('connection', (socket) => {
